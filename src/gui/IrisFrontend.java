688124b05c0d32eb3527eb0accd698ed52358bcc
kritikos <kritikos@embl.de>
2016-10-25 16:13:33 +0200

    before code cleanup

--- src/gui/IrisFrontend.java
+++ src/gui/IrisFrontend.java
@@ -100,12 +100,12 @@ public class IrisFrontend {
 	/**
 	 * This string holds the software version that is defined here once to be used whenever it needs to be displayed.
 	 */
-	public static String IrisVersion = "0.9.5.18";
+	public static String IrisVersion = "0.9.6.1";
 
 	/**
 	 * This string holds the hash id of Iris versioning in Git
 	 */
-	public static String IrisBuild = "320330e";
+	public static String IrisBuild = "b142b9c";
 
 
 	/**
